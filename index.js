4fdf6ebbcd32cb9ea24e95eb6fdfd1132d83ab56
Matheus <rbiam027@BKUSL-1EWFML7H.local>
2020-08-15 11:55:51 -0400

    set up express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || 3000;
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.listen(PORT, () =>
+  console.log(`Server is up and running at port ${PORT} 🚀`)
+);
